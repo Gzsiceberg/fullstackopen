a548ede10d6dc88b2f56c88bb743aba68795cf63
gzs_iceberg <gzs_iceberg@outlook.com>
2026-01-09 12:15:00 +0800

    feat: add phone number field to person entries in phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -5,25 +5,31 @@ const Persons = (props) => {
   return (
     <div>
       {persons.map(person =>
-        <p key={person.name}>{person.name}</p>
+        <p key={person.name}>{person.name} {person.number}</p>
       )}
     </div>)
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567' }
   ])
   const [newName, setNewName] = useState('')
-
+  
   let handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const [newPhoneNumber, setNewPhoneNumber] = useState('')
+  let handlePhoneNumberChange = (event) => {
+    setNewPhoneNumber(event.target.value)
   }
 
   let onSubmit = (event) => {
     event.preventDefault()
     const personObject = {
-      name: newName
+      name: newName,
+      number: newPhoneNumber
     }
     if (persons.some(person => person.name === newName)) {
       alert(`${newName} is already added to phonebook`)
@@ -31,7 +37,9 @@ const App = () => {
     }
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewPhoneNumber('')
   }
+
   return (
     <div>
       <h2>Phonebook</h2>
@@ -39,6 +47,7 @@ const App = () => {
         <div>
           name: <input value={newName} onChange={handleNameChange} />
         </div>
+        <div>number: <input value={newPhoneNumber} onChange={handlePhoneNumberChange} /></div>
         <div>
           <button type="submit">add</button>
         </div>
