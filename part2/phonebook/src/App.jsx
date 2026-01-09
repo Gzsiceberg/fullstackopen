aa8c39f7f49df1eb2673cde8be5bfb609e4213da
gzs_iceberg <gzs_iceberg@outlook.com>
2026-01-09 12:10:15 +0800

    feat: initialize phonebook app with React and Vite
    - Added package.json with dependencies and scripts for development
    - Included Vite logo SVG in the public directory
    - Created main application component (App.jsx) to manage phonebook entries
    - Implemented state management for adding new contacts
    - Set up entry point (main.jsx) for rendering the App component
    - Configured Vite with a basic configuration file (vite.config.js)

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+
+const Persons = (props) => {
+  let persons = props.persons
+  return (
+    <div>
+      {persons.map(person =>
+        <p key={person.name}>{person.name}</p>
+      )}
+    </div>)
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  let handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  let onSubmit = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={onSubmit}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <Persons persons={persons} />
+    </div>
+  )
+}
+
+export default App
