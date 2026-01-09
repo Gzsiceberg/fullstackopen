a3c703e13c14f6521b90f303a21717bab53fb730
gzs_iceberg <gzs_iceberg@outlook.com>
2026-01-09 11:51:36 +0800

    feat: refactor App component to use Course and Content components for better structure

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -21,41 +21,48 @@ const Part = (props) => {
 }
 
 const Content = (props) => {
+  let parts = props.parts
   return (
     <div>
-      <Part part={props.parts[0]} />
-      <Part part={props.parts[1]} />
-      <Part part={props.parts[2]} />
+      {parts.map(part => <Part key={part.id} part={part} />)}
+    </div>
+  )
+}
+
+const Course = (props) => {
+  return (
+    <div>
+      <Header course={props.course.name} />
+      <Content parts={props.course.parts} />
+      {/* <Total parts={props.course.parts} /> */}
     </div>
   )
 }
 
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
 
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
+  return <Course course={course} />
 }
 
 export default App
