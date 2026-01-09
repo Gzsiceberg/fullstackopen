d3c024af9dfd63faf6b2fcc73e8f5085286660b1
gzs_iceberg <gzs_iceberg@outlook.com>
2026-01-09 21:48:26 +0800

    feat: remove CORS middleware and dependency from the backend

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -4,9 +4,6 @@ app.use(express.json())
 
 app.use(express.static('dist'))
 
-const cors = require('cors')
-app.use(cors())
-
 const morgan = require('morgan')
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
