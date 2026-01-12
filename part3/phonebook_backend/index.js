e9f2ab93480042f6a0763382cb1291e91311d6e9
gzs_iceberg <gzs_iceberg@outlook.com>
2026-01-12 13:57:25 +0800

    feat: integrate MongoDB for phonebook management and add dotenv for environment variables

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,4 +1,6 @@
+require('dotenv').config()
 const express = require('express')
+const Phonebook = require('./models/phonebook')
 const app = express()
 app.use(express.json())
 
@@ -8,28 +10,6 @@ const morgan = require('morgan')
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    {
-        "id": "1",
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": "2",
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": "3",
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": "4",
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
@@ -37,34 +17,45 @@ app.get('/', (request, response) => {
 
 app.get('/api/persons/:id', (request, response) => {
     const id = request.params.id
-    const person = persons.find(person => person.id === id)
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+    Phonebook.findById(id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => {
+            console.log(error)
+            response.status(500).end()
+        })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
     const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+    Phonebook.findByIdAndRemove(id)
+        .then(() => {
+            response.status(204).end()
+        })
+        .catch(error => {
+            console.log(error)
+            response.status(500).end()
+        })
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Phonebook.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get('/info', (request, response) => {
-    const date = new Date()
-    const info = `<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`
-    response.send(info)
+    Phonebook.find({}).then(persons => {
+        const date = new Date()
+        const info = `<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`
+        response.send(info)
+    })
 })
-
-const generateId = () => {
-    return String(Math.floor(Math.random() * 10000) + 1000)
-}
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
@@ -87,21 +78,28 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    const hasExistingName = persons.find(person => person.name === body.name)
-    if (hasExistingName) {
-        return response.status(400).json({
-            error: 'name must be unique'
+    Phonebook.find({ name: body.name }).then(existingPersons => {
+        if (existingPersons.length > 0) {
+            return response.status(400).json({
+                error: 'name must be unique'
+            })
+        }
+
+        const person = new Phonebook({
+            name: body.name,
+            number: body.number,
         })
-    }
 
-    const person = {
-        name: body.name,
-        number: body.number,
-        id: generateId(),
-    }
-
-    persons = persons.concat(person)
-    response.json(person)
+        person.save().then(savedPerson => {
+            response.json(savedPerson)
+        }).catch(error => {
+            console.log(error)
+            response.status(500).end()
+        })
+    }).catch(error => {
+        console.log(error)
+        response.status(500).end()
+    })
 })
 
 const PORT = process.env.PORT || 3001
