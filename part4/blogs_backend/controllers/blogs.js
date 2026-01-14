bb4f475d21f0176b69810c363702aa37ca2f6369
gzs_iceberg <gzs_iceberg@outlook.com>
2026-01-14 12:12:18 +0800

    feat: associate blogs with users and validate userId in blog creation

--- part4/blogs_backend/controllers/blogs.js
+++ part4/blogs_backend/controllers/blogs.js
@@ -1,8 +1,9 @@
 const router = require('express').Router()
 const Blogs = require('../models/blogs')
+const Users = require('../models/users')
 
 router.get('/', async (request, response) => {
-    const blog = await Blogs.find({})
+    const blog = await Blogs.find({}).populate('userId', { username: 1, name: 1 })
     response.json(blog)
 })
 
@@ -38,15 +39,27 @@ router.post('/', async (request, response) => {
         })
     }
 
+    const user = await Users.findById(body.userId)
+    if (!user) {
+        return response.status(400).json({
+            error: 'userId missing or invalid'
+        })
+    }
+
     if (!body.title || !body.url) {
         return response.status(400).json({
             error: 'title or url missing'
         })
     }
 
-    const person = new Blogs(request.body)
-    const result = await person.save()
-    response.status(201).json(result)
+    const blog = new Blogs(request.body)
+    blog.user = user._id
+    const saveBlog = await blog.save()
+
+    user.blogs = user.blogs.concat(saveBlog._id)
+    await user.save()
+
+    response.status(201).json(saveBlog)
 })
 
 module.exports = router
