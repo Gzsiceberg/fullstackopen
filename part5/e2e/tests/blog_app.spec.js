a1f0cf05993614f17b4c21d7ca7ce2acb9e44e2b
gzs_iceberg <gzs_iceberg@outlook.com>
2026-01-15 15:48:45 +0800

    feat: add beforeEach hook to improve test organization in blog app tests

--- part5/e2e/tests/blog_app.spec.js
+++ part5/e2e/tests/blog_app.spec.js
@@ -1,8 +1,20 @@
-const { test, describe, expect } = require('@playwright/test')
+const { test, describe, expect, beforeEach } = require('@playwright/test')
 
 describe('blog app', () => {
-  test('front page can be opened', async ({ page }) => {
+  beforeEach(async ({ page }) => {
     await page.goto('http://localhost:5173')
+  })
+
+  test('front page can be opened', async ({ page }) => {
     await expect(page.getByLabel('username')).toBeVisible()
+    await expect(page.getByLabel('password')).toBeVisible()
+  })
+
+  test('user can log in', async ({ page }) => {
+    await page.getByRole('button', { name: 'login' }).click()
+    await page.getByLabel('username').fill('root')
+    await page.getByLabel('password').fill('sekret')
+    await page.getByRole('button', { name: 'login' }).click()
+    await expect(page.getByText('Superuser logged in')).toBeVisible()
   })
 })
