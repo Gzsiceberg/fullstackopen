dca5f8887684860ee1ef2dd4e856693dd759595e
gzs_iceberg <gzs_iceberg@outlook.com>
2026-01-22 14:10:17 +0800

    feat: implement add book functionality with GraphQL mutation and auto-refresh

--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -39,3 +39,15 @@ export const EDIT_AUTHOR = gql`
     }
   }
 `
+
+export const ADD_BOOK = gql`
+  mutation addBook($title: String!, $author: String!, $published: Int!, $genres: [String!]!) {
+    addBook(title: $title, author: $author, published: $published, genres: $genres) {
+      title
+      published
+      author
+      genres
+      id
+    }
+  }
+`
