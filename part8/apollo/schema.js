4c2c6e7a286c36d772e493bc5d1e65b4a0c2881d
gzs_iceberg <gzs_iceberg@outlook.com>
2026-01-22 15:09:20 +0800

    feat: restructure Apollo server setup with resolvers and schema, and update dependencies

---
+++ part8/apollo/schema.js
@@ -0,0 +1,38 @@
+const typeDefs = /* GraphQL */ `
+  type Query {
+    bookCount: Int!
+    authorCount: Int!
+    allAuthors: [Author!]!
+    allBooks(author: String, genre: String): [Book!]!
+  }
+
+  type Author {
+    name: String!
+    bookCount: Int!
+    born: Int
+    id: ID!
+  }
+
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    genres: [String!]!
+    id: ID!
+  }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
+  }
+`
+
+module.exports = typeDefs
